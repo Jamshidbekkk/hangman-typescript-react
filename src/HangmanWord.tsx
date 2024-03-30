9b1f72600d7dbed9bbff944b49a21b6c8ae31de4
Jamshidbek <kamoliddinovjamshid7@gmail.com>
2024-03-30 16:43:06 +0500

    functionality done

--- src/HangmanWord.tsx
+++ src/HangmanWord.tsx
@@ -1,18 +1,41 @@
+type HangmanWordProps = {
+  guessedLetters: string[]
+  wordToGuess: string
+  reveal?: boolean
+}
 
-const HangmanWord = () => {
-    const word = "test";
-    const guessedLetters = ["t"];
-  
-    return (
-      <div style={{ display: "flex", gap: ".25em", fontSize: "6rem", fontWeight: "bold", textTransform: "uppercase", fontFamily: "monospace" }}>
-        {word.split("").map((letter, index) => (
-            <span key={index} style={{ borderBottom: ".1em solid black" }}>
-                <span style={{visibility: guessedLetters.includes(letter) ? "visible" : "hidden",}}>{letter}</span>
-            </span>
-        ))}
-      </div>
-    );
-  };
-  
-  export default HangmanWord;
-  
+export function HangmanWord({
+  guessedLetters,
+  wordToGuess,
+  reveal = false,
+}: HangmanWordProps) {
+  return (
+    <div
+      style={{
+        display: "flex",
+        gap: ".25em",
+        fontSize: "6rem",
+        fontWeight: "bold",
+        textTransform: "uppercase",
+        fontFamily: "monospace",
+      }}
+    >
+      {wordToGuess.split("").map((letter, index) => (
+        <span style={{ borderBottom: ".1em solid black" }} key={index}>
+          <span
+            style={{
+              visibility:
+                guessedLetters.includes(letter) || reveal
+                  ? "visible"
+                  : "hidden",
+              color:
+                !guessedLetters.includes(letter) && reveal ? "red" : "black",
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  )
+}
